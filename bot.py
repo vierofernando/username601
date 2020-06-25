c60749cbba39dcd4b4078aa6eca981bc8b4eb671
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2020-06-25 23:35:18 +0700

    Updated files for public version of Username601

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import modules.canvas as Painter
 
 # EXTERNAL PACKAGES
 import os
+from subprocess import run, PIPE
 from inspect import isawaitable
 import dbl
 
@@ -822,6 +823,19 @@ async def on_message(message):
                                 await message.channel.send('**Requested by '+req+':** Deleted '+str(len(deleted_messages))+' messages in <#'+str(message.channel.id)+'>.\nSpecifically for messages by <@'+str(message.mentions[0].id)+'>.', delete_after=3)
                             except Exception as e:
                                 await message.channel.send(str(client.get_emoji(BotEmotes.error)) + ' | An error occured during purging. ```'+str(e)+'```')
+        if args[0]==prefix+'bash':
+            if int(message.author.id)==Config.owner.id:
+                try:
+                    if no_args: raise OSError('you are gay')
+                    if len(unprefixed.split())==1:
+                        data = run([unprefixed], stdout=PIPE).stdout.decode('utf-8')
+                    else:
+                        data = run([unprefixed.split()[0], unprefixed[1:len(unprefixed)]], stdout=PIPE).stdout.decode('utf-8')
+                    await message.channel.send(embed=discord.Embed(title='Bash Terminal', description='Input:```sh\n'+str(unprefixed)+'```**Output:**```sh\n'+str(data)+'```', color=discord.Color.green()))
+                except Exception as e:
+                    await message.channel.send(embed=discord.Embed(title='Error on B45H', description='Input:```sh\n'+str(unprefixed)+'```**Error:**```py\n'+str(e)+'```', color=discord.Color.red()))
+            else:
+                await message.channel.send(embed=discord.Embed(title='Error on B45H', description='Input:```sh\n'+str(unprefixed)+'```**Error:**```py\nDenied by username601.sh```', color=discord.Color.red()))
         if args[0]==prefix+'ex' or args[0]==prefix+'eval':
             if int(message.author.id)==Config.owner.id:
                 command = unprefixed
@@ -829,9 +843,9 @@ async def on_message(message):
                     res = eval(command)
                     if 'token' in unprefixed: res = 'YO MAMA'
                     if isawaitable(res):
-                        await message.channel.send(embed=discord.Embed(title='Evaluation Success', description='Input:```py\n'+unprefixed+'```\nOutput:```py\n'+str(await res)+'```Return type:```py\n'+str(type(await res))+'```', color=discord.Colour.green()))
+                        await message.channel.send(embed=discord.Embed(title='Evaluation Success', description='Input:```py\n'+unprefixed+'```**Output:**```py\n'+str(await res)+'```Return type:```py\n'+str(type(await res))+'```', color=discord.Colour.green()))
                     else:
-                        await message.channel.send(embed=discord.Embed(title='Evaluation Success', description='Input:```py\n'+unprefixed+'```\nOutput:```py\n'+str(res)+'```Return type:```py\n'+str(type(res))+'```', color=discord.Color.green()))
+                        await message.channel.send(embed=discord.Embed(title='Evaluation Success', description='Input:```py\n'+unprefixed+'```**Output:**```py\n'+str(res)+'```Return type:```py\n'+str(type(res))+'```', color=discord.Color.green()))
                 except Exception as e:
                     await message.channel.send(embed=discord.Embed(title='Evaluation Caught an Exception', description='Input:```py\n'+unprefixed+'```\nException:```py\n'+str(e)+'```', color=discord.Colour.red()))
             else:
