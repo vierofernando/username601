7f605de04377f5f3a4a0fba7be462d9bdcf54e6d
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2020-06-28 22:45:38 +0700

    epic

--- heroku-tools.py
+++ heroku-tools.py
@@ -14,7 +14,7 @@ def main():
         resp = input('Do you have git installed in your system?\nAnd use automatic heroku deployments from a github repo?\n(y/n) >>>')
         if 'y' in resp.lower():
             with open("update.bat", "w+") as f:
-                f.write(" git add . && git commit -a -m \"Updated files and stuff (AUTO)\" && git push >> update.cmd")
+                f.write("git add . && git commit -a -m \"Updated files and stuff (AUTO)\" && git push")
                 f.close()
         else: print("No? OK then.")
     print("Done.")
