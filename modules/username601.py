d15bbb3938e47ea011cb527b83bac6ef3c66b6c1
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2020-09-19 13:50:06 +0700

    this is an automatic update. (13:50:02,37 - 19/09/2020 UTC+7)

--- modules/username601.py
+++ modules/username601.py
@@ -46,11 +46,6 @@ def ping():
     return round((t.now().timestamp()-a)*1000)
 
 def getUserAvatar(ctx, args, size=1024, user=None, allowgif=False):
-    if list(args)[0].startswith('<') and list(args)[0].endswith('>'):
-        res = list(args)[0][:-1][1:]
-        temp = list(args)
-        temp[0] = res
-        args = tuple(temp)
     if len(list(args))==0:
         if len(ctx.message.attachments) > 0:
             if ctx.message.attachments[0].filename.split('.')[::-1][0].lower() in ['webp', 'png', 'jpg', 'jpeg']:
@@ -58,6 +53,11 @@ def getUserAvatar(ctx, args, size=1024, user=None, allowgif=False):
         if allowgif: return str(ctx.author.avatar_url).replace('.webp?size=1024', '.png?size'+str(size))
         else: return str(ctx.author.avatar_url).replace('.gif', '.webp').replace('.webp?size=1024', '.png?size'+str(size))
     elif len(list(args))==1 and list(args)[0].startswith('http'):
+        if list(args)[0].startswith('<') and list(args)[0].endswith('>'):
+            res = list(args)[0][:-1][1:]
+            temp = list(args)
+            temp[0] = res
+            args = tuple(temp)
         if list(args)[0].split('.')[::-1][0].lower() in ['png', 'webp', 'jpg', 'jpeg']:
             return list(args)[0]
     if len(ctx.message.mentions)>0:
