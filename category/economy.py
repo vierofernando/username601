d7191ad3d9ff4d970a7fe672dbea6507cbc3248c
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2020-09-21 09:35:33 +0700

    fixed several discord.colour shit

--- category/economy.py
+++ category/economy.py
@@ -48,7 +48,7 @@ class economy(commands.Cog):
         try:
             data = self.client.db.Economy.getBuyList(source)
             assert data['error']==False, data['ctx']
-            return await ctx.send(embed=discord.Embed(title='{}\'s buy list'.format(source.name), description=data['ctx'], color=discord.Colour.from_rgb(201, 160, 112)))
+            return await ctx.send(embed=discord.Embed(title='{}\'s buy list'.format(source.name), description=data['ctx'], color=self.client.utils.get_embed_color(discord)))
         except Exception as e:
             return await ctx.send('{} | {}!'.format(self.client.utils.emote(self.client, 'error'), str(e)))
 
@@ -104,7 +104,7 @@ class economy(commands.Cog):
             assert data['error']==False, data['ctx']
             em = discord.Embed(title='{}\'s shop'.format(ctx.guild.name), description='\n'.join(
                 [str(i+1)+'. **'+str(data['ctx'][i]['name'])+'** (price: '+str(data['ctx'][i]['price'])+' :gem:)' for i in range(len(data['ctx']))]
-            ), color=discord.Colour.from_rgb(201, 160, 112))
+            ), color=self.client.utils.get_embed_color(discord))
             return await ctx.send(embed=em)
         except Exception as e:
             await ctx.send('{} | {}!\nYou can always add a value using `{}addproduct <price> <name>`'.format(self.client.utils.emote(self.client, 'error'), str(e), self.client.utils.prefix))
