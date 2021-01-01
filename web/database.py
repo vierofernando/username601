178b54ce5aa50f7eacc7e28c058a56d5a79561f2
Viero Fernando <60427892+vierofernando@users.noreply.github.com>
2021-01-01 17:46:50 +0700

    add caching system to reduce database calls

--- web/database.py
+++ web/database.py
@@ -11,6 +11,8 @@ class DB:
             3600: "hour",
             60: "minute"
         }
+        self.cache = None
+        self.last_usage = time()
 
     def strfsecond(self, seconds):
         seconds = int(seconds)
@@ -28,9 +30,12 @@ class DB:
         return f"{seconds} year" + ("" if seconds == 1 else "s")
     
     def get_data(self):
+        if self.cache and ((time() - self.last_usage) <= 61):
+            return self.cache
+
         data = self.db.find_one({"h": True})
         stats = data["stats"].split(":")
-        return {
+        self.cache = {
             "guild_count": stats[0],
             "users_count": stats[1],
             "economy_length": stats[2],
@@ -38,3 +43,5 @@ class DB:
             "last_update": self.strfsecond(time() - float(stats[4])),
             "changelog": data["changelog"]
         }
+        self.last_usage = time()
+        return self.cache
